5f5e445debb769981bdee04d58249a4662f08612
David <david@MacAlejandro.local>
2023-02-06 12:34:49 +0100

    productos

--- src/html/_data/config.js
+++ src/html/_data/config.js
@@ -1,5 +1,26 @@
 module.exports = [
     {
-
-    }
+        imagen: "/images/blazer-low-jumbo.png" ,
+        producto: "Nike Blazer Low Jumbo `77" ,
+        precio: "99,99" ,
+        precioOferta: "59,97"
+    } ,
+    {
+        imagen: "/images/wearallday.png" ,
+        producto: "Nike Wearallday" ,
+        precio: "69,99" ,
+        precioOferta: "41,97"
+    },
+    {
+        imagen: "/images/pegasus-turbo.png" ,
+        producto: "Nike Pegasus Turbo Next Nature" ,
+        precio: "159,99" ,
+        precioOferta: "95,97"
+    },
+    {
+        imagen: "" ,
+        producto: "" ,
+        precio: "" ,
+        precioOferta: ""
+    }, 
 ]
