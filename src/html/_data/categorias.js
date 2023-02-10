324b93461f88b4bcd0c1ecacff4bbc7c926810c2
David <david@MacAlejandro.local>
2023-02-10 11:35:22 +0100

    data

--- src/html/_data/categorias.js
+++ src/html/_data/categorias.js
@@ -109,9 +109,115 @@ module.exports = [
                 tallas: [38, 40, 42.5, 43, 44, 45, 47]
             } , 
             {
-                imagen: "/images/nike-hombre/blazer-low-jumbo-77/1.png" ,
-                nombre: "Nike Blazer Low Jumbo '77" ,
-                descripcion: "Dicen que no vale la pena cambiar lo que ya funciona, y por nosotros bien. Las superestrellas clásicas del estilo urbano se renuevan en las Nike Blazer Low '77 Jumbo. Ofrecen el look clásico que más te gusta con un diseño Swoosh oversize y unos cordones enormes. Su lengüeta de espuma suave y las costuras más gruesas destacan el look icónico alabado por el estilo urbano desde el 77.",
+                id: 4 ,
+                imagen: "/images/nike-hombre/pegasus-turbo-next-nature/1.png" ,
+                nombre: "Nike Pegasus Turbo Next Nature" ,
+                descripcion: "Deja que este Pegaso te impulse durante los momentos más complicados de la carrera. Con una espuma reciclada, más ligera y reactiva y una parte superior de plumas, este modelo está confeccionado para aumentar el ritmo sin sacrificar la comodidad mientras alcanzas tu mejor marca personal. Este modelo inmortal está confeccionado de manera responsable con, al menos, un 50 % de material reciclado por peso, para mantener su filosofía de eficacia demostrada para el día a día",
+                categoria: [
+                    {
+                        id: 4 ,
+                        nombre: "Hombre" ,
+                        productos: [] ,
+                        hijos: [
+                            {
+                                id: 4 ,
+                                nombre: "Hombre" ,
+                                productos: [] ,
+                                hijos: [] ,
+                                filtros: [{
+                                    id: 4 ,
+                                    nombre: "Talla" ,
+                                    valores: ["XL" , "XXL"]
+                                }]
+                            }
+                        ] ,
+                        filtros: [{
+                            id: 4 ,
+                            nombre: "Talla" ,
+                            valores: ["XL" , "XXL"]
+                        }]
+                    }
+                ] ,
+                galeria: ["/images/nike-hombre/pegasus-turbo-next-nature/2.png", "/images/nike-hombre/pegasus-turbo-next-nature/3.png", "/images/nike-hombre/pegasus-turbo-next-nature/4.png"] ,
+                precio: 159.99 ,
+                precioOferta: "" ,
+                tallas: [39, 40, 42, 43, 44, 46, 47] 
+            } ,   
+            {
+                id: 5 , 
+                imagen: "/images/nike-hombre/blazer-mid-jumbo-77/1.png" ,
+                nombre: "Nike Blazer Mid Jumbo '77" ,
+                descripcion: "Alabadas por muchos por su aspecto y su sensación de resistencia, el básico de armario se actualiza con las Nike Blazer Mid ' 77 Jumbo.Aprovecha el look clásico que más te gusta, que ahora cuenta con un talón elástico con textura de pana y unas presillas grandes para que puedas ponértelas y quitártelas fácilmente.El diseño Swoosh extragrande y los cordones enormes añaden un toque divertido" ,
+                categoria: [
+                    {
+                        id: 5 ,
+                        nombre: "Hombre" ,
+                        productos: [] ,
+                        hijos: [
+                            {
+                                id: 5 ,
+                                nombre: "Hombre" ,
+                                productos: [] ,
+                                hijos: [] ,
+                                filtros: [{
+                                    id: 5 ,
+                                    nombre: "Talla" ,
+                                    valores: ["XL" , "XXL"]
+                                }]
+                            }
+                        ] ,
+                        filtros: [{
+                            id: 5 ,
+                            nombre: "Talla" ,
+                            valores: ["XL" , "XXL"]
+                        }]
+                    }
+                ] ,
+                galeria: ["/images/nike-hombre/blazer-mid-jumbo-77/2.png", "/images/nike-hombre/blazer-mid-jumbo-77/3.png", "/images/nike-hombre/blazer-mid-jumbo-77/4.png"],
+                precio: 109.99 ,
+                precioOferta: 65.97 ,
+                tallas: [36, 38.5, 40, 41, 42, 44, 47] 
+            } ,
+            {
+                id: 6 ,
+                imagen: "/images/nike-hombre/air-max-systm/1.png" ,
+                nombre: "Nike Air Max SYSTM" ,
+                descripcion: "Los looks Max, con sensación Max. Las Air Max SYSTM recuperan todo lo que te gustan de tu rollo favorito de los 80 (sin el pantalón de paracaídas). La amortiguación Nike Air probada y visible se combina con una parte superior elegante inspirada en el deporte. Air Max cumple de nuevo" ,
+                categoria: [
+                    {
+                        id: 6 ,
+                        nombre: "Hombre" ,
+                        productos: [] ,
+                        hijos: [
+                            {
+                                id: 6 ,
+                                nombre: "Hombre" ,
+                                productos: [] ,
+                                hijos: [] ,
+                                filtros: [{
+                                    id: 6 ,
+                                    nombre: "Talla" ,
+                                    valores: ["XL" , "XXL"]
+                                }]
+                            }
+                        ] ,
+                        filtros: [{
+                            id: 6 ,
+                            nombre: "Talla" ,
+                            valores: ["XL" , "XXL"]
+                        }]
+                    }
+                ] ,
+                galeria: ["/images/nike-hombre/air-max-systm/2.png", "/images/nike-hombre/air-max-systm/3.png", "/images/nike-hombre/air-max-systm/4.png"] ,
+                precio: 99.99 ,
+                precioOferta: 59.97 ,
+                tallas: [39, 40, 42, 43, 45, 46]
+            }  ,
+            {
+                id: 7 ,
+                imagen: "/images/nike-hombre/air-more-uptempo-96/1.png" ,
+                nombre: "Nike Air More Uptempo '96" ,
+                descripcion: "El diseño atrevido y llamativo es exclusivo del estilo A-I-R. Con un diseño inspirado en el baloncesto de altos vuelos y el arte del grafiti de los 90, estas zapatillas son el centro de atención. Además, las unidades Max Air del talón, el antepié y el mediopié proporcionan una amortiguación cómoda para que tu aspecto esté a la par de tu comodidad" ,
                 categoria: [
                     {
                         id: 1 ,
@@ -137,13 +243,83 @@ module.exports = [
                         }]
                     }
                 ] ,
-                galeria: ["/images/nike-hombre/blazer-low-jumbo-77/2.png", "/images/nike-hombre/blazer-low-jumbo-77/3.png", "/images/nike-hombre/blazer-low-jumbo-77/4.png"],
-                precio: 99.95 ,
-                precioOferta: 54.95,
-                tallas: ["40", "41", "42", "43", "46", "48", "52"] 
-            } ,                                    
+                galeria: ["/images/nike-hombre/air-more-uptempo-96/2", "/images/nike-hombre/air-more-uptempo-96/3", "/images/nike-hombre/air-more-uptempo-96/4"],
+                precio: 179.99 ,
+                precioOferta: 107.97 ,
+                tallas: [42.5, 43, 45, 46, 48] 
+            }  ,
+            {
+                id: 8 ,
+                imagen: "/images/nike-hombre/air-force-1-low-retro/1.png" ,
+                nombre: "Nike Air Forcr 1 Low Retro" ,
+                descripcion: "Da el sí quiero (de nuevo) a unas zapatillas que querrás llevar para el resto de tu vida. Las Nike Air Force 1 Low Retro recuperan los materiales originales y mantienen todo lo que más te gusta: un diseño de piel clásica y la cantidad perfecta de estilo de baloncesto para rendir homenaje a un icono de la moda para el día a día. Ahora, incorporan un cepillo de limpieza para mantener un look impecable",
+                categoria: [
+                    {
+                        id: 8 ,
+                        nombre: "Hombre" ,
+                        productos: [] ,
+                        hijos: [
+                            {
+                                id: 8 ,
+                                nombre: "Hombre" ,
+                                productos: [] ,
+                                hijos: [] ,
+                                filtros: [{
+                                    id: 8 ,
+                                    nombre: "Talla" ,
+                                    valores: ["XL" , "XXL"]
+                                }]
+                            }
+                        ] ,
+                        filtros: [{
+                            id: 8 ,
+                            nombre: "Talla" ,
+                            valores: ["XL" , "XXL"]
+                        }]
+                    }
+                ] ,
+                galeria: ["/images/nike/-hombre/air-force-1-low-retro/2.png", "/images/nike/-hombre/air-force-1-low-retro/3.png", "/images/nike/-hombre/air-force-1-low-retro/4.png"] ,
+                precio: 149.99 ,
+                precioOferta: 119.97 ,
+                tallas: [36, 38, 38.5, 39, 43, 45] ,
+            },
+            {
+                id: 9 ,
+                imagen: "/images/nike-hombre/react-miler-3/1.png" ,
+                nombre: "Nike React Miler 3" ,
+                descripcion: "Amortiguación y comodidad, como debería ser cada carrera diaria. Las Nike React Miler 3 ofrecen suavidad cuando los pies pisan el asfalto. Hemos añadido estabilidad en el talón para ofrecer seguridad, y la tracción duradera te ayuda en las rutas cortas y largas" ,
+                categoria: [
+                    {
+                        id: 9 ,
+                        nombre: "Hombre" ,
+                        productos: [] ,
+                        hijos: [
+                            {
+                                id: 9 ,
+                                nombre: "Hombre" ,
+                                productos: [] ,
+                                hijos: [] ,
+                                filtros: [{
+                                    id: 9 ,
+                                    nombre: "Talla" ,
+                                    valores: ["XL" , "XXL"]
+                                }]
+                            }
+                        ] ,
+                        filtros: [{
+                            id: 9 ,
+                            nombre: "Talla" ,
+                            valores: ["XL" , "XXL"]
+                        }]
+                    }
+                ] ,
+                galeria: ["/images/nike-hombre/react-miler-3/2.png", "/images/nike-hombre/react-miler-3/3.png", "/images/nike-hombre/react-miler-3/4.png"] ,
+                precio: 129.99 ,
+                precioOferta: 77.97 ,
+                tallas: [41, 42, 47.5, 49.5] 
+            },                           
         ] ,
-        hijos: [
+        hijos:  [
             {
                 id: 1 ,
                 nombre: "Camisetas hombre" ,
@@ -159,7 +335,7 @@ module.exports = [
         filtros: [{
             id: 1 ,
             nombre: "Talla" ,
-            valores: ["XL" , "XXL"]
+            valores: ["S", "M", "L", "XL", "XXL"]
         }]
     },
     {
